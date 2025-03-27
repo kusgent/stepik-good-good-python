4c89b43d991276fafdad6327b51f42018c0dd2e0
Kustov Ilya <kusgent@yandex.ru>
2025-03-27 22:07:01 +0300

    Add solutions for section_03\lesson_07

---
+++ section_03/lesson_07/step_04.py
@@ -0,0 +1,5 @@
+# https://stepik.org/lesson/567031/step/4?unit=561305
+
+v = [1205, 1101, 1434, 1320, 923, 874]
+
+print(v[2:])
---
+++ section_03/lesson_07/step_07.py
@@ -0,0 +1,5 @@
+# https://stepik.org/lesson/567031/step/7?unit=561305
+
+m = [2, 3, 5, 5, 2, 2, 3, 3, 4, 5, 4, 4]
+
+print(m[6:1:-1])
